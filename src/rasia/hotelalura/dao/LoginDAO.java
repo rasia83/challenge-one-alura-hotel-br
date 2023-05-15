5e435ae1b7217a326354ca603e143c9c6a81c593
Tiago Edgar Rasia <tiago_rasia@hotmail.com>
2023-05-15 10:55:32 -0300

    correção de erro , não retornava msg de erro do login

--- src/rasia/hotelalura/dao/LoginDAO.java
+++ src/rasia/hotelalura/dao/LoginDAO.java
@@ -3,6 +3,7 @@ package rasia.hotelalura.dao;
 import java.security.NoSuchAlgorithmException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import rasia.hotelalura.util.EncryptPassword;
 
@@ -17,19 +18,24 @@ public class LoginDAO {
 	public boolean passwordTest(String username, String password) {
 
 		String jpql = "SELECT l.password FROM Login l WHERE l.username = :username";
-		String dbPass = em.createQuery(jpql, String.class)
-						.setParameter("username", username)
-						.getSingleResult();
 
 		try {
+			String dbPass = em.createQuery(jpql, String.class)
+							.setParameter("username", username)
+							.getSingleResult();
+			
 			password = EncryptPassword.encrypt(password);
+			
+			if(password.equalsIgnoreCase(dbPass))
+				return true;
+			
+		} catch (NoResultException e) {
+			return false;
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 		
-		if(password.equalsIgnoreCase(dbPass))
-			return true;
 		return false;
 	}
 
